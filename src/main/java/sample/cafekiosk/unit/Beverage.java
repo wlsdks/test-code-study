fb303860e9702c80e2aa0d4eaf981273214b4331
wlsdks <wlsdks12@naver.com>
2023-07-30 02:32:07 +0900

    기본 단위테스트 코드 추가

---
+++ src/main/java/sample/cafekiosk/unit/Beverage.java
@@ -0,0 +1,8 @@
+package sample.cafekiosk.unit;
+
+public interface Beverage {
+
+    String getName();
+    int getPrice();
+
+}
--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,5 +1,12 @@
 package sample.cafekiosk.unit;
 
 public class CafeKiosk {
-    
+
+    public void add(Beverage beverage) {
+
+    }
+
+    public int calculateTotalPrice() {
+        return 0;
+    }
 }
