ed7be2d974fbec2fd2690e1b7c65816f4abecdef
wlsdks <wlsdks12@naver.com>
2023-07-30 02:35:06 +0900

    1. 메서드 추가

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,12 +1,30 @@
 package sample.cafekiosk.unit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CafeKiosk {
 
-    public void add(Beverage beverage) {
+    private final List<Beverage> beverages = new ArrayList<>();
 
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
     }
 
     public int calculateTotalPrice() {
-        return 0;
+        int totalPrice = 0;
+        for (Beverage beverage : beverages) {
+            totalPrice += beverage.getPrice();
+        }
+        return totalPrice;
     }
+
 }
