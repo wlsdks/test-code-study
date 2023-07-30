839aa7ec476bb66a632d7daab97c8d18acc97a8b
wlsdks <wlsdks12@naver.com>
2023-07-30 20:07:30 +0900

    1. 스프링부트에 jpa를 활용한 프로젝트 도메인 추가

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/BaseEntity.java
@@ -0,0 +1,26 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * 엔티티 생성 및 변경마다 이게 동작해서 시간을 남긴다.
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+
+}
---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,30 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber; // 상품번호
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type; // 상품타입
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingType sellingType; // 판매타입
+
+    private String name; // 상품명
+
+    private int price; // 가격
+
+
+}
