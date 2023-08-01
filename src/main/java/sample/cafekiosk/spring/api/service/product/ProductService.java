1338aebfc4f3fe881cca07f8df898e96c281894c
wlsdks <wlsdks12@naver.com>
2023-08-02 08:19:11 +0900

    1. 상품서비스 테스트코드 작성 완료

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -11,18 +12,47 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기전용이다.
+ * CRUD에서 CUD는 동작X / only Read
+ * JPA : CUD 스냅샷 저장, 변경감지X (성능이 향상됨)
+ *
+ * CQRS - Command / Query -> Command Query Responsibility seperate 서로의 책임분리
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductCreateRequest request) {
-        // productNumber
-        // 001 002 003 004
-        // DB에서 마지막 저장된 Product의 상품번호를 읽어와서 +1한다.
-        // 009 -> 010
+    /**
+     * 상품 추가 - 동시성 문제
+     */
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        // DB에서 마지막 저장된 Product의 상품번호를 읽어와서 +1한다.  009 -> 010
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
+    /**
+     * 저장할 다음 상품번호 생성
+     */
+    private String createNextProductNumber() {
         String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
     }
 
     public List<ProductResponse> getSellingProducts() {
@@ -32,4 +62,6 @@ public class ProductService {
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
     }
+
+
 }
