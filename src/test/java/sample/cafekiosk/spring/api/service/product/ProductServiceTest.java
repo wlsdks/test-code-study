19a96aa318640e26578daa4335b935d46d2e02b5
wlsdks <wlsdks12@naver.com>
2023-08-02 17:21:14 +0900

    1. 테스트코드 통합테스트환경 통일을 위한 작업 실시

--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -2,9 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.IntegrationTestSupport;
 import sample.cafekiosk.spring.api.service.product.request.ProductServiceCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -19,9 +17,7 @@ import static org.assertj.core.api.Assertions.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
-@ActiveProfiles("test")
-@SpringBootTest
-class ProductServiceTest {
+class ProductServiceTest extends IntegrationTestSupport {
 
     @Autowired
     private ProductService productService;
